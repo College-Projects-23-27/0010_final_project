4989f5002294a461c8f493fb3631a8d98b0fb201
Valyrn <valhernr@gmail.com>
2023-11-15 16:22:53 -0500

    Converted to function so it can be used later

--- primary_care_access.py
+++ primary_care_access.py
@@ -1,24 +1,25 @@
 import pandas as pd
 import fpsnippets
 
-data = pd.read_csv("data-primary-care-access-facilities.csv")
+def get_most_providers():
+    data = pd.read_csv("data-primary-care-access-facilities.csv")
 
-neighborhoods = {}
+    neighborhoods = {}
 
-# For each primary care facility, get the nighberhood. If it is in the dictionary of neighborhoods, add it to the list. If it is not, then create a new key
-# Ignore if it is not in Pittsburgh
-for i in range(len(data)):
-    data_point = data.iloc[i]
+    # For each primary care facility, get the nighberhood. If it is in the dictionary of neighborhoods, add it to the list. If it is not, then create a new key
+    # Ignore if it is not in Pittsburgh
+    for i in range(len(data)):
+        data_point = data.iloc[i]
 
-    neighborhood = fpsnippets.geo_to_neighborhood(data_point['Latitude'], data_point['Longitude'])
+        neighborhood = fpsnippets.geo_to_neighborhood(data_point['Latitude'], data_point['Longitude'])
 
-    if neighborhood is not None:
-        try:
-            neighborhoods[neighborhood].append(data_point['GROUP_NAME'])
-        except:
-            neighborhoods[neighborhood] = [data_point['GROUP_NAME']]
+        if neighborhood is not None:
+            try:
+                neighborhoods[neighborhood].append(data_point['GROUP_NAME'])
+            except:
+                neighborhoods[neighborhood] = [data_point['GROUP_NAME']]
 
-list_of_neighborhoods = list(neighborhoods.keys())
-list_of_neighborhoods.sort(key=lambda element: len(neighborhoods[element]))
-list_of_neighborhoods.reverse()
-print([(hood, len(neighborhoods[hood])) for hood in list_of_neighborhoods])
+    list_of_neighborhoods = list(neighborhoods.keys())
+    list_of_neighborhoods.sort(key=lambda element: len(neighborhoods[element]))
+    list_of_neighborhoods.reverse()
+    return[(hood, len(neighborhoods[hood])) for hood in list_of_neighborhoods]
