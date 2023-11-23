e88bfefe39ba25ccc00be60ee2e3c6369f775f06
Wafik Yaacoub <73697128+Wafik20@users.noreply.github.com>
2023-11-22 19:03:38 -0500

    Delete graphical_neighborhood.py

--- graphical_neighborhood.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plot
-from lead_levels.lead_level import get_blood_lead_levels
-
-def graph_data(neighborhoods, values):
-    plot.bar(neighborhoods, values)
-    plot.xticks(rotation=90, fontsize='x-small')
-    plot.autoscale(enable=True, axis='x', tight=False)
-    plot.tight_layout()
-    plot.show()
